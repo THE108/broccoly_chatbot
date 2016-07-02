dcea56d8eaf5e6511a3dfa21fe1a2332047ff276
Rustam Zagirov <rustam.zagirov@lazada.com>
2016-07-02 22:01:13 +0300

    return gem

--- config/initializers/messenger.rb
+++ config/initializers/messenger.rb
@@ -1,9 +1,9 @@
-# unless Rails.env.production?
-#   Dir["#{Rails.root}/app/bot/**/*.rb"].each { |file| require file }
-# end
-#
-# Facebook::Messenger.configure do |config|
-#   config.access_token = ENV['MESSENGER_PAGE_ACCESS_TOKEN']
-#   config.app_secret = ENV['MESSENGER_APP_SECRET']
-#   config.verify_token = ENV['MESSENGER_VERIFY_TOKEN']
-# end
+unless Rails.env.production?
+  Dir["#{Rails.root}/app/bot/**/*.rb"].each { |file| require file }
+end
+
+Facebook::Messenger.configure do |config|
+  config.access_token = ENV['MESSENGER_PAGE_ACCESS_TOKEN']
+  config.app_secret = ENV['MESSENGER_APP_SECRET']
+  config.verify_token = ENV['MESSENGER_VERIFY_TOKEN']
+end
